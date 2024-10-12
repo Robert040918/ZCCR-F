657b7796507f41e9cafc60d34f86aa3c6cecaf43
Robert040918 <1042860182@qq.com>
2024-10-12 21:14:43 +0800

    添加Mob场景和Main场景

--- player.gd
+++ player.gd
@@ -1,4 +1,5 @@
 extends Area2D
+signal hit
 
 @export var speed = 400 # 玩家角色的速度
 var screen_size
@@ -7,8 +8,52 @@ var screen_size
 func _ready():
 	# 获取游戏窗口大小
 	screen_size = get_viewport_rect().size
-
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+	# 玩家向量移动
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	
+	# 速度归一化
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	# 移动位置
+	position += velocity * delta
+	# 限制移动范围
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	# 选择动画
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# 向左移动水平翻转
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	if velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		# 向下移动垂直翻转
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	hide() # 碰撞后消失
+	hit.emit()
+	# 禁用碰撞检测
+	$CollisionShape2D.set_deferred("disabled",true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
