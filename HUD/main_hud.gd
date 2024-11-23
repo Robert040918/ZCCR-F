ffbe493a4da4368a4af2aee8dfadb054b2f31295
Robert040918 <1042860182@qq.com>
2024-11-23 22:02:13 +0800

    启用Dialogic 2插件，测试对话

--- HUD/main_hud.gd
+++ HUD/main_hud.gd
@@ -1,8 +1,17 @@
 extends Node
 
+
+func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_text_signal)
+	Dialogic.start("res://HUD/leader.dtl")
+	
+func _on_dialogic_text_signal(argument:String):
+	if argument == "toquit":
+		get_tree().quit()
+
+
 func _on_minigame_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://smallgames/dodge_the_creeps/main.tscn")
-
 
 func _on_war_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://War/Field/example.tscn")
