d70903e6a128b0512d14fd5e03afd324cb63c44f
Robert040918 <1042860182@qq.com>
2024-12-01 20:18:13 +0800

    继续组织项目文件夹，添加信号可视化插件

--- HUD/main_hud.gd
+++ HUD/main_hud.gd
@@ -3,7 +3,7 @@ extends Node
 
 func _ready() -> void:
 	Dialogic.signal_event.connect(_on_dialogic_text_signal)
-	Dialogic.start("res://HUD/leader.dtl")
+	Dialogic.start("res://Tests/Dialogs/leader.dtl")
 	
 func _on_dialogic_text_signal(argument:String):
 	if argument == "toquit":
@@ -11,7 +11,7 @@ func _on_dialogic_text_signal(argument:String):
 
 
 func _on_minigame_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://smallgames/dodge_the_creeps/main.tscn")
+	get_tree().change_scene_to_file("res://minigames/dodge_the_creeps/main.tscn")
 
 func _on_war_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://War/Field/example.tscn")
