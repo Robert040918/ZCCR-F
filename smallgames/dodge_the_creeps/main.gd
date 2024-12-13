d5b33fd1fea166cbc0e9431ce72f0ce4a927e034
Robert040918 <1042860182@qq.com>
2024-12-13 21:51:20 +0800

    重置

---
+++ smallgames/dodge_the_creeps/main.gd
@@ -0,0 +1,67 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#new_game()
+	$HUD.toload()
+	
+func game_over():
+	$GameTimer.stop()
+	$MobTimer.stop()
+	$Music.stop()
+	$HUD.show_game_over()
+	$DeathSound.play()
+	$HUD.score_group.append(score)
+	$HUD.tosave()
+	$HUD.toload()
+	#print($HUD.score_group)
+	
+func new_game():
+	$Music.play()
+	score = 0
+	$HUD.toload()
+	get_tree().call_group("mobs", "queue_free")
+	$player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.update_best_score()
+	$HUD.show_message("在按键上移动手指就可移动")
+	await $HUD/MessageTimer.timeout
+	$HUD.show_message("Get Ready")
+
+func _on_mob_timer_timeout():
+	# 创建怪物实例
+	var mob = mob_scene.instantiate()
+	
+	# 让怪物在路径上选择随机位置
+	var mob_spawn_location = $MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio = randf()
+	
+	# 让怪物在路径上向右旋转
+	var direction = mob_spawn_location.rotation + PI / 2
+	mob.position = mob_spawn_location.position
+	# 随机旋转角度
+	direction += randf_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	# 随机选择怪物的移动速度
+	var velocity = Vector2(randf_range(100.0, 250.0), 0.0)
+	mob.linear_velocity = velocity.rotated(direction)
+	
+	# 将新实例添加到场景中
+	add_child(mob)
+
+
+func _on_game_timer_timeout():
+	score += 1
+	$MobTimer.wait_time -= 0.01
+	$HUD.update_score(score)
+	
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$GameTimer.start()
+	$MobTimer.wait_time = 2.0
+	
---
+++ smallgames/dodge_the_creeps/player/player.gd
@@ -0,0 +1,60 @@
+extends Area2D
+signal hit
+
+@export var speed = 350 # 玩家角色的速度
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# 获取游戏窗口大小
+	screen_size = get_viewport_rect().size
+	hide()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	# 玩家向量移动
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	
+	# 速度归一化
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	# 移动位置
+	position += velocity * delta
+	# 限制移动范围
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	# 选择动画
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# 向左移动水平翻转
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	if velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		# 向下移动垂直翻转
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+
+func _on_body_entered(body):
+	await get_tree().create_timer(0.2).timeout
+	hide() # 碰撞后消失
+	hit.emit()
+	# 禁用碰撞检测
+	$CollisionShape2D.set_deferred("disabled",true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
